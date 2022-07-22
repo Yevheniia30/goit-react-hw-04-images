17309a11256ddd9c351afa4d4b584e457d03e141
Yevheniia Suska <susskaya86@gmail.com>
2022-07-22 23:34:26 +0300

    from classes to hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 // import axios from 'axios';
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import { Searchbar } from './Searchbar';
 import { Modal } from './Modal/Modal';
 import { Loader } from './Loader';
@@ -8,118 +8,255 @@ import { Button } from './Button';
 
 import { getImagesReq } from 'services/api';
 
-export class App extends Component {
-  state = {
-    images: [],
-    isLoading: false,
-    searchQuery: '',
-    error: null,
-    page: 1,
-    isModalImage: '',
-    isAltModalImage: '',
-    total: 0,
-  };
+export const App = () => {
+  const [images, setImages] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('');
+  const [page, setPage] = useState(1);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [isModalImage, setisModalImage] = useState('');
+  const [isAltModalImage, setisAltModalImage] = useState(true);
+  const [total, setTotal] = useState(0);
 
-  // componentDidMount() {
-  // this.getImages();
+  useEffect(() => {
+    const getImages = async () => {
+      setIsLoading(true);
+
+      try {
+        const data = await getImagesReq({ searchQuery, page });
+
+        setImages(prev => [...prev, ...data.hits]);
+        // setPage(page => page + 1);
+
+        setTotal(data.totalHits);
+
+        window.scrollTo({
+          top: document.documentElement.scrollHeight,
+          behavior: 'smooth',
+        });
+      } catch (error) {
+        setError({ error });
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    if (searchQuery) {
+      getImages();
+    }
+  }, [page, searchQuery]);
+
+  //  componentDidUpdate(prevProps, prevState) {
+  //   if (prevState.searchQuery !== this.state.searchQuery) {
+  //     // console.log('new searrch', prevState, this.state);
+  //     this.getImages();
+  //   }
   // }
 
-  componentDidUpdate(prevProps, prevState) {
-    if (prevState.searchQuery !== this.state.searchQuery) {
-      // console.log('new searrch', prevState, this.state);
-      this.getImages();
-    }
-  }
+  // const getImages = async () => {
+  //   setIsLoading(true);
 
-  getImages = async () => {
-    const { searchQuery, page } = this.state;
-    this.setState({
-      isLoading: true,
-    });
-    try {
-      const data = await getImagesReq({ searchQuery, page });
+  //   try {
+  //     const data = await getImagesReq({ searchQuery, page });
 
-      // console.log('response', data.hits);
-      this.setState(prevState => ({
-        images: [...prevState.images, ...data.hits],
-        page: prevState.page + 1,
-        total: data.totalHits,
-      }));
+  //     setImages([...images, ...data.hits]);
+  //     setPage(page => page + 1);
+  //     setTotal(data.totalHits);
 
-      window.scrollTo({
-        top: document.documentElement.scrollHeight,
-        behavior: 'smooth',
-      });
-    } catch (error) {
-      this.setState({ error });
-    } finally {
-      this.setState({
-        isLoading: false,
-      });
-    }
-  };
+  //     window.scrollTo({
+  //       top: document.documentElement.scrollHeight,
+  //       behavior: 'smooth',
+  //     });
+  //   } catch (error) {
+  //     setError({ error });
+  //   } finally {
+  //     setIsLoading(false);
+  //   }
+  // };
 
-  onSubmit = query => {
+  const onSubmit = query => {
     console.log('query', query);
-    this.setState({
-      searchQuery: query,
-      page: 1,
-      images: [],
-    });
+    if (query === searchQuery) {
+      return;
+    }
+    // this.setState({
+    //   searchQuery: query,
+    //   page: 1,
+    //   images: [],
+    // });
+    setSearchQuery(query);
+    setPage(1);
+    setImages([]);
+    setError(null);
   };
 
-  onLoadClick = () => {
-    this.getImages();
+  const onLoadClick = () => {
+    // this.getImages();
+    // getImages();
+    setPage(page => page + 1);
   };
 
-  onOpenModal = e => {
+  const onOpenModal = e => {
     if (e.target.nodeName === 'IMG') {
-      this.setState({
-        isModalImage: e.target.dataset.modal,
-      });
+      // this.setState({
+      //   isModalImage: e.target.dataset.modal,
+      // });
+      setisModalImage(e.target.dataset.modal);
+      setisAltModalImage(e.target.alt);
     }
   };
 
-  onCloseModal = () => {
-    this.setState({
-      isModalImage: '',
-      isAltModalImage: '',
-    });
+  const onCloseModal = () => {
+    console.log('onClose');
+    // this.setState({
+    //   isModalImage: '',
+    //   isAltModalImage: '',
+    // });
+    setisModalImage('');
+    setisAltModalImage('');
   };
 
-  render() {
-    // console.log('images', this.state.images);
-    const { images, isLoading, error, isModalImage, isAltModalImage, total } =
-      this.state;
-    const { onSubmit, onOpenModal, onLoadClick, onCloseModal } = this;
-    // console.log('searchquery', this.state.searchQuery);
-    return (
-      <div
-        style={{
-          display: 'grid',
-          gridTemplateColumns: '1fr',
-          gridGap: '16px',
-          paddingBottom: '24px',
-        }}
-      >
-        <Searchbar onSubmit={onSubmit} />
-        {error ? (
-          <h2>Oops something went wrong...try again</h2>
-        ) : (
-          <ImageGallery data={images} onClickToOpenModal={onOpenModal} />
-        )}
-        {images.length && !isLoading && images.length < total ? (
-          <Button onClick={onLoadClick} />
-        ) : null}
-        {isLoading && <Loader />}
-        {isModalImage && (
-          <Modal
-            image={isModalImage}
-            altImage={isAltModalImage}
-            onClose={onCloseModal}
-          />
-        )}
-      </div>
-    );
-  }
-}
+  return (
+    <div
+      style={{
+        display: 'grid',
+        gridTemplateColumns: '1fr',
+        gridGap: '16px',
+        paddingBottom: '24px',
+      }}
+    >
+      <Searchbar onSubmit={onSubmit} />
+      {error ? (
+        <h2>Oops something went wrong...try again</h2>
+      ) : (
+        <ImageGallery data={images} onClickToOpenModal={onOpenModal} />
+      )}
+      {images.length && !isLoading && images.length < total ? (
+        <Button onClick={onLoadClick} />
+      ) : null}
+      {isLoading && <Loader />}
+      {isModalImage && (
+        <Modal
+          image={isModalImage}
+          altImage={isAltModalImage}
+          onClose={onCloseModal}
+        />
+      )}
+    </div>
+  );
+};
+
+// export class AppOld extends Component {
+//   state = {
+//     images: [],
+//     isLoading: false,
+//     searchQuery: '',
+//     error: null,
+//     page: 1,
+//     isModalImage: '',
+//     isAltModalImage: '',
+//     total: 0,
+//   };
+
+//   // componentDidMount() {
+//   // this.getImages();
+//   // }
+
+//   componentDidUpdate(prevProps, prevState) {
+//     if (prevState.searchQuery !== this.state.searchQuery) {
+//       // console.log('new searrch', prevState, this.state);
+//       this.getImages();
+//     }
+//   }
+
+//   getImages = async () => {
+//     const { searchQuery, page } = this.state;
+//     this.setState({
+//       isLoading: true,
+//     });
+//     try {
+//       const data = await getImagesReq({ searchQuery, page });
+
+//       // console.log('response', data.hits);
+//       this.setState(prevState => ({
+//         images: [...prevState.images, ...data.hits],
+//         page: prevState.page + 1,
+//         total: data.totalHits,
+//       }));
+
+//       window.scrollTo({
+//         top: document.documentElement.scrollHeight,
+//         behavior: 'smooth',
+//       });
+//     } catch (error) {
+//       this.setState({ error });
+//     } finally {
+//       this.setState({
+//         isLoading: false,
+//       });
+//     }
+//   };
+
+//   onSubmit = query => {
+//     console.log('query', query);
+//     this.setState({
+//       searchQuery: query,
+//       page: 1,
+//       images: [],
+//     });
+//   };
+
+//   onLoadClick = () => {
+//     this.getImages();
+//   };
+
+//   onOpenModal = e => {
+//     if (e.target.nodeName === 'IMG') {
+//       this.setState({
+//         isModalImage: e.target.dataset.modal,
+//       });
+//     }
+//   };
+
+//   onCloseModal = () => {
+//     this.setState({
+//       isModalImage: '',
+//       isAltModalImage: '',
+//     });
+//   };
+
+//   render() {
+//     // console.log('images', this.state.images);
+//     const { images, isLoading, error, isModalImage, isAltModalImage, total } =
+//       this.state;
+//     const { onSubmit, onOpenModal, onLoadClick, onCloseModal } = this;
+//     // console.log('searchquery', this.state.searchQuery);
+//     return (
+//       <div
+//         style={{
+//           display: 'grid',
+//           gridTemplateColumns: '1fr',
+//           gridGap: '16px',
+//           paddingBottom: '24px',
+//         }}
+//       >
+//         <Searchbar onSubmit={onSubmit} />
+//         {error ? (
+//           <h2>Oops something went wrong...try again</h2>
+//         ) : (
+//           <ImageGallery data={images} onClickToOpenModal={onOpenModal} />
+//         )}
+//         {images.length && !isLoading && images.length < total ? (
+//           <Button onClick={onLoadClick} />
+//         ) : null}
+//         {isLoading && <Loader />}
+//         {isModalImage && (
+//           <Modal
+//             image={isModalImage}
+//             altImage={isAltModalImage}
+//             onClose={onCloseModal}
+//           />
+//         )}
+//       </div>
+//     );
+//   }
+// }
--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,47 +1,94 @@
 import PropTypes from 'prop-types';
-import { Component } from 'react';
+import { useEffect } from 'react';
 import { createPortal } from 'react-dom';
 import s from './Modal.module.css';
 
-const modalRoot = document.getElementById('modal-root');
-const body = document.querySelector('body');
+export const Modal = ({ image, altImage, onClose }) => {
+  const modalRoot = document.getElementById('modal-root');
+  const body = document.querySelector('body');
 
-export class Modal extends Component {
-  componentDidMount() {
-    document.addEventListener('keydown', this.handleClose);
-    // if (this.props.image) {
-    body.style.overflow = 'hidden';
-    // }
-    // if (!isOpen) body.style.overflow = 'auto';
-  }
-
-  componentWillUnmount() {
-    document.removeEventListener('keydown', this.handleClose);
-    body.style.overflow = 'auto';
-  }
-
-  handleClose = e => {
+  const handleOverlay = e => {
     if (e.target === e.currentTarget) {
-      this.props.onClose();
-      return;
-    }
-    if (e.code === 'Escape') {
-      this.props.onClose();
-      // return;
+      onClose();
     }
   };
 
-  render() {
-    return createPortal(
-      <div className={s.Overlay} onClick={this.handleClose}>
-        <div className={s.Modal}>
-          <img src={this.props.image} alt={this.props.altImage} />
-        </div>
-      </div>,
-      modalRoot
-    );
-  }
-}
+  useEffect(() => {
+    const handleClose = e => {
+      if (e.code === 'Escape') {
+        onClose();
+      }
+    };
+
+    window.addEventListener('keydown', handleClose);
+    // if (this.props.image) {
+    body.style.overflow = 'hidden';
+    return () => {
+      window.removeEventListener('keydown', handleClose);
+      body.style.overflow = 'auto';
+    };
+  }, [onClose, body]);
+
+  // const handleClose = e => {
+  //   if (e.target === e.currentTarget) {
+  //     onClose();
+  //     return;
+  //   }
+  //   if (e.code === 'Escape') {
+  //     onClose();
+  //     // return;
+  //   }
+  // };
+
+  return createPortal(
+    <div className={s.Overlay} onClick={handleOverlay}>
+      <div className={s.Modal}>
+        <img src={image} alt={altImage} />
+      </div>
+    </div>,
+    modalRoot
+  );
+};
+
+// const modalRoot = document.getElementById('modal-root');
+// const body = document.querySelector('body');
+
+// export class ModalOld extends Component {
+//   componentDidMount() {
+//     document.addEventListener('keydown', this.handleClose);
+//     // if (this.props.image) {
+//     body.style.overflow = 'hidden';
+//     // }
+//     // if (!isOpen) body.style.overflow = 'auto';
+//   }
+
+//   componentWillUnmount() {
+//     document.removeEventListener('keydown', this.handleClose);
+//     body.style.overflow = 'auto';
+//   }
+
+//   handleClose = e => {
+//     if (e.target === e.currentTarget) {
+//       this.props.onClose();
+//       return;
+//     }
+//     if (e.code === 'Escape') {
+//       this.props.onClose();
+//       // return;
+//     }
+//   };
+
+//   render() {
+//     return createPortal(
+//       <div className={s.Overlay} onClick={this.handleClose}>
+//         <div className={s.Modal}>
+//           <img src={this.props.image} alt={this.props.altImage} />
+//         </div>
+//       </div>,
+//       modalRoot
+//     );
+//   }
+// }
 
 Modal.propTypes = {
   image: PropTypes.string.isRequired,
